7791974c3d851231cd2041369471bffe393d17a2
Prevergenec <prevergenec@ya.ru>
2022-12-24 14:16:54 +0300

    add code, class

--- script.js
+++ script.js
@@ -5,13 +5,24 @@ btn.forEach(item => {
 		console.log(item.dataset.click);
 		item.dataset.click++;
 			
-		if (item.dataset.click == 5) {
+		if (item.dataset.click == 4) {
 			console.log('done');
 			
 			btn.forEach(item => {
 				item.setAttribute('disabled', 'true');
 			});
 		}
+
+		let user = {
+			name: 'Mike',
+			age: 41
+		}
+
+		let addAttrUser = item.dataset.user = JSON.stringify(user);
+
+		console.log(addAttrUser);
+
+		console.log(JSON.parse(addAttrUser));
 		
 	});
 
